a48ab7e2386bcab6e6e132dcdd39ba873bd5063c
Hagan103 <haganj@mail.com>
2019-06-28 16:32:11 +0930

    Initial commit

---
+++ project.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Page Title</title>
+		<meta name="description" content="My portfolio">
+		
+		<link href="https://fonts.googleapis.com/css?family=Montserrat:100&display=swap" rel="stylesheet">
+		
+		<link href="css/bootstrap.css" rel="stylesheet">
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
+
+		<link href="css/portfolio.css" rel="stylesheet">
+
+		
+		<script src="js/jquery-3.4.1.min.js"></script>  
+		<script src="js/bootstrap.min.js"></script>  
+		
+		<style>
+		[class^="col"] {
+  padding-top: 1rem;
+  padding-bottom: 1rem;
+}
+
+
+@media (min-width: 1200px) {
+
+  .col-xl-4 {
+  float:left;
+    width: 100% !important;
+  }
+
+}
+.row{
+	
+	padding:0 !important;
+	margin:0; !important;
+}
+		</style>
+		
+	</head>
+	<body>
+<nav class="navbar navbar-default" role="navigation">
+	<div class="container">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header"> 
+    </div>
+<ul>
+  <li><a href="#home">Home</a></li>
+    <li><a href="#about">Portfolio</a></li>
+  <li><a href="#contact">Contact</a></li>
+  	    <li><a href="#github"><i class="fab fa-github fa-2x"></i></a></li>
+  	    <li><a href="#github"><i class="fab fa-codepen fa-2x"></i></a></li>
+
+</ul>
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="navbar-collapse-1">
+     
+   
+    </div>
+    </div>
+
+</nav>
+<div class="nav-space"></div>
+<div class="project-article" style="background:">
+	<!-- <div class="project-header" style="	background:url(portfolio/toribash-textures-header.png) "></div> -->
+<div class="container">
+	<div class="row">
+  <div class="col-xl-8 project-image-container" style="background:pink">  
+  <img src="portfolio/toribash-textures.png" alt="project-piece">
+	  </div>
+  <div class="col-xl-4 project-about container-fluid" style="background:#fcfcfc; color:#464646;"> 
+  <h1> Toribash Forum </h1>
+  <hr>
+  <dl class="project-about-info">
+  <dt>Project</dt>
+  <dd>Toribash Forum</dd>
+  <dt>Client</dt>
+  <dd>Nabi Studios</dd>
+   <dt>Role</dt>
+  <dd>Nabi Studios</dd>
+   <dt>Technologies Used</dt>
+  <dd>Nabi Studios</dd>
+</dl>
+					  <div class="row">
+    <div class="col-md-6"><a href="">Preview</a></div>
+    <div class="col-md-6"><a href="https://github.com/Hagan103">Github <i class="fab fa-github fa-2x"></i></a>					</a></div>
+  </div>
+
+<hr>
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi nec lectus sit amet ipsum cursus finibus vel eu diam. Fusce sed tellus non velit mattis laoreet at at ipsum. Pellentesque vehicula ultricies mi at placerat. Aliquam sollicitudin congue blandit. Quisque ultrices tempor sem id euismod. Proin diam nisl, tempus nec arcu ac, dignissim tempus turpis. Suspendisse tincidunt bibendum venenatis. Vestibulum ipsum nibh, egestas at ex non, ornare bibendum enim. Nulla mollis pulvinar sapien. Nunc ultrices, libero pretium pharetra aliquam, dolor erat mattis orci, rutrum fermentum purus felis ut ipsum. Integer viverra interdum felis. Sed lobortis lorem in ex venenatis ullamcorper. Mauris quis fermentum nisi.
+
+Aliquam erat volutpat. Duis non dolor vitae odio commodo malesuada. Fusce eget lectus lacus. Cras tempus ultrices orci ac auctor. Mauris eros eros, eleifend nec leo a, facilisis iaculis nisi. In at est dui. Maecenas aliquam enim at arcu mollis sollicitudin. Vivamus dolor enim, condimentum vitae diam nec, efficitur vestibulum enim. Proin ac sapien orci. Proin mattis erat eu sollicitudin fringilla. Proin dignissim vitae nisi vel hendrerit. In non sem diam.
+
+Suspendisse volutpat porta sollicitudin. Duis tempor felis orci, at lacinia velit maximus id. Sed at dolor quis massa maximus ornare sed vitae odio. Suspendisse vulputate mi nec blandit pulvinar. Donec id augue vitae felis ullamcorper imperdiet. Suspendisse ut lectus venenatis, varius dolor ac, molestie enim. Integer arcu diam, aliquet id lorem ut, rhoncus lacinia felis. Proin placerat tortor eget massa lacinia volutpat. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. In placerat et enim in feugiat. Proin porta ex felis, vitae rhoncus nibh imperdiet quis.
+</div>
+
+</div>
+</div>
+
+
+		
+	</body>
+</html>
